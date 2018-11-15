0b6610b3cf909df4f6b8444eaff2ff4e3aa32446
IvanaGoSt <ivanagost@zoho.com>
2018-11-15 21:20:26 +0100

    Add colors

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 
+
 const container = document.querySelector("#container");
 
-let arrOfSigns = ["car", "camera-retro", "ambulance", "anchor", "balance-scale", "bath", "bed", "beer", "bell", "bicycle", "binoculars", "birthday-cake", "bomb", "bug", "bus", "coffee", "cut", "dice", "drum", "envelope", "flask", "futbol", "gem", "gift", "glasses", "home", "lemon", "paperclip"];
+let arrOfSigns = [["car", "blue"], ["camera-retro", "green"] ,["ambulance", "red"], ["anchor", "DodgerBlue"], ["balance-scale", "DarkOrange"], ["bath", "DarkViolet"], ["bed", "MediumOrchid"], ["beer", "OrangeRed"], ["bell", "MediumVioletRed"], ["bicycle", "seagreen"], ["binoculars", "purple"], ["birthday-cake", "Sienna"], ["bomb", "DarkSlateGrey"], ["bug", "DarkOliveGreen"], ["bus", "PaleVioletRed"], ["coffee", "brown"], ["cut", "DarkCyan"], ["dice", "violet"], ["drum", "SteelBlue"], ["envelope", "Tomato"], ["flask", "OliveDrab"], ["futbol", "MediumSlateBlue"], ["gem", "Magenta"], ["gift", "HotPink"], ["glasses", "MidnightBlue"], ["home", "RebeccaPurple"], ["lemon", "yellow"], ["paperclip", "Teal"]];
 
 let cardIsClickable = true;
 let arrOfPairs = [];
@@ -18,7 +19,7 @@ function displayCards(num) {
   let selection = document.getElementById("selection");
 let numberOfCards = Number(selection.value);
 
-  while(container.firstChild){    container.removeChild(container.firstChild);// make new grid of cards
+  while(container.firstChild){    container.removeChild(container.firstChild);// clear previous grid of cards
 }
   num = numberOfCards;
   giveRandomCards(num);
@@ -44,7 +45,7 @@ let numberOfCards = Number(selection.value);
   for(let i = 0; i < num; i++) {
     let card = document.createElement("div");
     card.classList.add("cards");
-    card.style.cssText = "border: 1px solid black; background-color: lightgray; display: flex; justify-content: center; align-items: center; font-size: 2em";
+    card.style.cssText = "border: 3px solid black; border-radius: 5%; box-shadow: 3px 3px 3px gray; background-color: lightgray; display: flex; justify-content: center; align-items: center; font-size: 2em";
     container.appendChild(card);
   }
   container.style.cssText = ("grid-template-columns: repeat(" + columns + ", 1fr); grid-template-rows: repeat(" + rows + ", 1fr)");
@@ -55,9 +56,9 @@ function giveRandomCards(num) {
   let arrOfCards = [];
 
   for(let i = 0; i < num / 2; i++) {
-    arrOfCards.push(arrOfSigns[i]);
-    arrOfPairs = arrOfCards.concat(arrOfCards);
-     arrOfPairs.sort(function(a, b){return 0.5 - Math.random()});
+    arrOfCards.push(arrOfSigns[i]);// pick signs
+    arrOfPairs = arrOfCards.concat(arrOfCards);// make two cards of each sign
+     arrOfPairs.sort(function(a, b){return 0.5 - Math.random()});// randomize cards
   }
 }
 
@@ -77,11 +78,14 @@ function openCards() {
     function open() {
       if(cards[i].innerHTML != "") {
         return;
-      }
+      }// prevent clicking the opened cards
       if(cardIsClickable === true){
-        cards[i].innerHTML = "<i class='fas fa-" + arrOfPairs[i] + "'></i>";
-      openedCards.push(arrOfPairs[i]);
-      }
+        cards[i].innerHTML = "<i class='fas fa-" + arrOfPairs[i][0] + "'></i>";
+        cards[i].style.color = arrOfPairs[i][1];
+
+    openedCards.push(arrOfPairs[i]);
+      };
+
 
      if(openedCards.length === 2) {
        if(openedCards[0] !== openedCards[1]) {
@@ -96,6 +100,7 @@ cards[arrOfPairs.lastIndexOf(openedCards[1])].innerHTML = "";
          }
        }else {
          openedPairs.push(openedCards[0], openedCards[1]);
+        // cards[i].style.border = "5px solid orange";
          openedCards.splice(0, 2);
         }
       }
